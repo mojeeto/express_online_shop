5cece6fd952768af120cbc660307b1ed8b1d25bf
Mojeeto <mojtaba01nouri@gmail.com>
2023-10-25 15:24:57 +0330

    fetch, delete, update, create product logic

--- controller/products.ts
+++ controller/products.ts
@@ -2,43 +2,79 @@ import Product from "../models/product";
 import controller from "./controller";
 
 export const getProducts: controller = (req, res, next) => {
-  res.render("index", {
-    pageTitle: "Home",
-    path: "/",
-    products: [],
-  });
+  Product.findAll()
+    .then((products) => {
+      res.render("index", {
+        pageTitle: "Home",
+        path: "/",
+        products,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 export const getManageProduct: controller = (req, res, next) => {
-  res.render("pages/admin/products/manage-products", {
-    fixTitle: "Manage Product",
-    path: "/admin/manage-products",
-    products: [],
-  });
+  Product.findAll()
+    .then((products) => {
+      res.render("pages/admin/products/manage-products", {
+        fixTitle: "Manage Product",
+        path: "/admin/manage-products",
+        products,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 export const postAddProduct: controller = (req, res, next) => {
-  const {
-    productName: name,
-    productPrice: price,
-    productDescription: description,
-    productImage: imageUrl,
-  } = req.body;
-  res.status(301).redirect("/admin/manage-products");
+  const { productName, productPrice, productDescription, productImage } =
+    req.body;
+  Product.create({
+    title: productName,
+    description: productDescription,
+    price: productPrice,
+    imageUrl: productImage,
+  })
+    .then((result) => {
+      res.status(301).redirect("/admin/manage-products");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 export const postUpdateProduct: controller = (req, res, next) => {
-  const {
-    productName: name,
-    productPrice: price,
-    productDescription: description,
-    productImage: imageUrl,
-  } = req.body;
+  const { productName, productPrice, productDescription, productImage } =
+    req.body;
   const productId = +req.params.id;
-  res.status(200).redirect("/admin/manage-products");
+  Product.findByPk(productId)
+    .then((product) => {
+      if (!product) throw new Error("product not found");
+      product.title = productName;
+      product.price = +productPrice;
+      product.description = productDescription;
+      product.imageUrl = productImage;
+      return product.save();
+    })
+    .then((result) => {
+      res.status(200).redirect("/admin/manage-products");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 export const getDeleteProduct: controller = (req, res, next) => {
   const productId = +req.params.id;
-  res.status(200).redirect("/admin/manage-products");
+  Product.findByPk(productId)
+    .then((product) => product?.destroy())
+    .then((result) => {
+      res.status(200).redirect("/admin/manage-products");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
--- models/product.ts
+++ models/product.ts
@@ -1,20 +1,42 @@
-import { DataTypes } from "sequelize";
+import {
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+} from "sequelize";
 import sequelize from "../utils/database";
 
-const Product = sequelize.define("product", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
+class Product extends Model<
+  InferAttributes<Product>,
+  InferCreationAttributes<Product>
+> {
+  declare id?: number;
+  declare title: string;
+  declare price: number;
+  declare description: string;
+  declare imageUrl: string;
+}
+
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    title: DataTypes.STRING,
+    price: {
+      type: DataTypes.DOUBLE,
+      allowNull: false,
+    },
+    description: DataTypes.STRING,
+    imageUrl: DataTypes.STRING,
   },
-  title: DataTypes.STRING,
-  price: {
-    type: DataTypes.DOUBLE,
-    allowNull: false,
-  },
-  description: DataTypes.STRING,
-  imageUrl: DataTypes.STRING,
-});
+  {
+    modelName: "product",
+    sequelize,
+  }
+);
 
 export default Product;
