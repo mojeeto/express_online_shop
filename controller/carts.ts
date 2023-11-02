b9306864d8269d3a83ca03e36190ee5e1fe974b8
Mojeeto <mojtaba01nouri@gmail.com>
2023-11-02 11:17:06 +0330

    add to cart and get cart products

--- controller/carts.ts
+++ controller/carts.ts
@@ -1,16 +1,42 @@
+import Product, { IProduct, isProduct } from "../models/product";
 import controller from "./controller";
 
 export const getCart: controller = (req, res, next) => {
-  res.status(200).render("pages/cart/cart", {
-    pageTitle: "Cart",
-    cartProducts: [],
-    totalPrice: 0,
-  });
+  req
+    .user!.populate("cart.products._id")
+    .then((user) => {
+      const products = user.cart.products.map((product) => {
+        if (isProduct(product._id)) {
+          return {
+            ...product._id,
+            count: product.count,
+            cost: +(product._id.price * product.count).toFixed(2),
+          };
+        }
+      });
+      res.status(200).render("pages/cart/cart", {
+        pageTitle: "Cart",
+        cartProducts: products,
+        totalPrice: user.cart.totalPrice,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 export const postAddProductToCart: controller = (req, res, next) => {
   const { productId } = req.params;
-  res.status(200).redirect("/cart");
+  Product.findById(productId)
+    .then((product) => {
+      return req.user!.addToCart(product!);
+    })
+    .then((user) => {
+      res.status(200).redirect("/cart");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 export const postRemoveProductFromCart: controller = (req, res, next) => {
