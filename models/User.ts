dd2fe6d7d4a119cb9f40bf0116ad15d3d322f5d4
Mojeeto <mojtaba01nouri@gmail.com>
2023-11-02 11:17:50 +0330

    change ejs cart ui and add method add to cart to user model

--- models/User.ts
+++ models/User.ts
@@ -1,8 +1,21 @@
-import { Document, InferSchemaType, Schema, model } from "mongoose";
+import { Document, PopulatedDoc, Schema, model } from "mongoose";
+import { IProduct } from "./product";
+
+type IUserCartProductItem = {
+  _id: PopulatedDoc<IProduct & Document>;
+  count: number;
+};
+
+type IUserCart = {
+  products: IUserCartProductItem[];
+  totalPrice: number;
+};
 
 export interface IUser extends Document {
   name: string;
   email: string;
+  cart: IUserCart;
+  addToCart: (product: IProduct) => Promise<IUser>;
 }
 
 const UserSchema = new Schema<IUser>({
@@ -14,8 +27,43 @@ const UserSchema = new Schema<IUser>({
     type: String,
     required: true,
   },
+  cart: {
+    products: [
+      {
+        _id: {
+          type: Schema.Types.ObjectId,
+          required: true,
+          ref: "Product",
+        },
+        count: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+  },
 });
 
-type UserTypeInfer = InferSchemaType<typeof UserSchema>;
+UserSchema.methods.addToCart = function (product: IProduct) {
+  const cart: IUserCart = {
+    products: [{ _id: product._id, count: 1 }],
+    totalPrice: product.price,
+  };
+  if (this.cart.products.length > 0) {
+    cart.products = this.cart.products;
+    cart.totalPrice = this.cart.totalPrice;
+    cart.products.forEach((ep) => {
+      if (ep._id.equals(product._id)) ep.count++;
+      cart.totalPrice += product.price;
+    });
+  }
+  cart.totalPrice = +cart.totalPrice.toFixed(2);
+  this.cart = cart;
+  return this.save();
+};
 
 export default model<IUser>("User", UserSchema);
