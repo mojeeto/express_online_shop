6f96b563bbb7f31ab79ef3fdff89285562773ba4
Mojeeto <mojtaba01nouri@gmail.com>
2023-11-02 20:22:23 +0330

    add login ui

--- controller/auth.ts
+++ controller/auth.ts
@@ -1,5 +1,5 @@
 import controller from "./controller";
 
 export const getLogin: controller = (req, res, next) => {
-  res.render("pages/auth/login", { pageTitle: "Login", path: "/login" });
+  res.render("pages/auth/login", { pageTitle: "Login", path: "/auth/login" });
 };
