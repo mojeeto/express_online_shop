b36225064d6f7451f3a9fca81c90a2d7dacec251
Mojeeto <mojtaba01nouri@gmail.com>
2023-11-03 10:54:03 +0330

    add bcryptjs and hash password

--- controller/auth.ts
+++ controller/auth.ts
@@ -1,5 +1,6 @@
 import controller from "./controller";
 import User from "../models/user";
+import { compare, hash } from "bcryptjs";
 
 export const getLogin: controller = (req, res, next) => {
   res.render("pages/auth/login", {
@@ -11,13 +12,21 @@ export const getLogin: controller = (req, res, next) => {
 
 export const postLogin: controller = (req, res, next) => {
   const { email, password } = req.body;
-  User.findOne({ email, password })
+  User.findOne({ email })
     .then((user) => {
-      req.session.isAuthenticated = true;
-      req.session.user = user;
-      req.session.save((err) => {
-        res.redirect("/");
-      });
+      if (!user) return res.redirect("/auth/login");
+      compare(password, user.password)
+        .then((isMatch) => {
+          if (isMatch) {
+            req.session.isAuthenticated = true;
+            req.session.user = user;
+            return req.session.save((err) => res.redirect("/"));
+          }
+          res.redirect("/auth/login");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     })
     .catch((err) => {
       console.log(err);
@@ -36,12 +45,12 @@ export const postSignup: controller = (req, res, next) => {
   const { email, password } = req.body;
   User.findOne({ email })
     .then((user) => {
-      if (!user) {
-        const newUser = new User({ email, password });
-        return newUser.save();
-      }
+      if (!user) return hash(password, 12);
       res.redirect("/auth/signup");
     })
+    .then((hashedPassword) =>
+      new User({ email, password: hashedPassword }).save()
+    )
     .then((user) => {
       res.redirect("/auth/login");
     })
